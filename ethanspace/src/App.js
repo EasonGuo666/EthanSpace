5ee86495902c3c0ad1147e6712ebb2697075a023
Yixuan <2761683494@qq.com>
2022-09-04 12:25:00 +0930

    Add the navbar

---
+++ ethanspace/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './components/NavBar'
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ ethanspace/src/components/NavBar.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import linkedInIcon from '../assets/img/nav-icon1.svg';
+import githubIcon from '../assets/img/nav-icon2.svg';
+import instagramIcon from '../assets/img/nav-icon3.svg';
+
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  // detect event and apply the effect on scrolling
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateLinks = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Navbar expand='lg' className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href='#home'>
+          <img src={logo} alt='Logo' />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav'>
+          <span className='navbar-toggler-icon'></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link
+              href='#home'
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateLinks("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href='#skills'
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateLinks("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href='#projects'
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateLinks("projects")}
+            >
+              Projects
+            </Nav.Link>
+          </Nav>
+          <span className='navbar-text'>
+            <div className='social-icon'>
+              <a href='https://www.linkedin.com/in/yixuan-guo-35372b1a9/'>
+                <img src={linkedInIcon} alt=''/>
+              </a>
+              <a href='https://github.com/EasonGuo666'>
+                <img src={githubIcon} alt='' />
+              </a>
+              <a href='https://www.instagram.com/guo_yixuan666/'>
+                <img src={instagramIcon} alt='' />
+              </a>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
